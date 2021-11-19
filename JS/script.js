5facbea40d724ec9b0acfd9bf88a2554e6fe032c
Kotoshilla <kotoshilla@gmail.com>
2021-11-19 20:01:48 +0600

    Циклы

--- JS/script.js
+++ JS/script.js
@@ -28,37 +28,65 @@
 
 
 
-if (4==9) { 
-  console.log('Верно!'); //если 4 равен 9 то выполниться эта команда
-} else { 
-  console.log('Error'); // в противном случае будеь выполнена эта строка кода
+// if (4==9) { 
+//   console.log('Верно!'); //если 4 равен 9 то выполниться эта команда
+// } else { 
+//   console.log('Error'); // в противном случае будеь выполнена эта строка кода
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//   console.log('Error');
+// } else if (num > 100) {
+//   console.log('Больше 100');
+// } else {
+//   console.log('Ok');
+// }
+
+// (num === 60) ? console.log('Ok') : console.log('Error'); //Тернарный оператор когода условий 3 
+
+// const numTwo = 45;
+
+// switch (numTwo) {
+//   case 49:
+//     console.log('Неверно');
+//     break;
+//   case 100:
+//     console.log('Неверно');
+//     break;
+//   case 15:
+//     console.log('В точку!');
+//     break;
+//   default:
+//     console.log('Ни один из кейсов не прошел');
+// }
+
+
+/*Циклы*/
+
+
+let num = 50;
+
+while (num <= 55) {
+  console.log(num);
+  num++;
 }
 
-const num = 50;
 
-if (num < 49) {
-  console.log('Error');
-} else if (num > 100) {
-  console.log('Больше 100');
-} else {
-  console.log('Ok');
+do {
+  console.log(num);
+  num++;
 }
+while (num < 55);
 
-(num === 60) ? console.log('Ok') : console.log('Error'); //Тернарный оператор когода условий 3 
 
-const numTwo = 45;
+let numTwo = 50;
 
-switch (numTwo) {
-  case 49:
-    console.log('Неверно');
-    break;
-  case 100:
-    console.log('Неверно');
-    break;
-  case 15:
-    console.log('В точку!');
-    break;
-  default:
-    console.log('Ни один из кейсов не прошел');
+for (let i = 1; i < 8; i++) {
+  if (i === 6) {
+    // break;
+    continue; //Пропускает шаг который нам не нужно но при эотм не прерывает цикл
+  }
+  console.log(i);
 }
-
