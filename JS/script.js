c90739b8d98554b7ee22d8da32b146c62b0d6d61
Kotoshilla <kotoshilla@gmail.com>
2021-11-19 16:42:23 +0600

    Начало

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+const numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMoveDB = {
+  count: numberOfFilm,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', '')
+      d = prompt('На сколько оцените его?', '');
+
+personalMoveDB.movies[a] = b;
+personalMoveDB.movies[c] = d;
+
+console.log(personalMoveDB);
+
