fe0989a11819e53c273858f7568142f934848150
Kotoshilla <kotoshilla@gmail.com>
2021-11-19 19:32:21 +0600

    Условия

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,64 @@
-const numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use stict";
 
-const personalMoveDB = {
-  count: numberOfFilm,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+// const numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', '')
-      d = prompt('На сколько оцените его?', '');
+// const personalMoveDB = {
+//   count: numberOfFilm,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
 
-personalMoveDB.movies[a] = b;
-personalMoveDB.movies[c] = d;
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', '')
+//       d = prompt('На сколько оцените его?', '');
 
-console.log(personalMoveDB);
+// personalMoveDB.movies[a] = b;
+// personalMoveDB.movies[c] = d;
+
+// console.log(personalMoveDB);
+
+
+
+
+//  /*Условия*/
+
+
+
+
+// if (4==9) { 
+//   console.log('Верно!'); //если 4 равен 9 то выполниться эта команда
+// } else { 
+//   console.log('Error'); // в противном случае будеь выполнена эта строка кода
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//   console.log('Error');
+// } else if (num > 100) {
+//   console.log('Больше 100');
+// } else {
+//   console.log('Ok');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error'); //Тернарный оператор когода условий 3 
+
+// const num = 45;
+
+// switch (num) {
+//   case 49:
+//     console.log('Неверно');
+//     break;
+//   case 100:
+//     console.log('Неверно');
+//     break;
+//   case 15:
+//     console.log('В точку!');
+//     break;
+//   default:
+//     console.log('Ни один из кейсов не прошел(');
+// }
 
