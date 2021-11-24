88b24397cfb7a60f89df529216e14bb73b892b87
Kotoshilla <kotoshilla@gmail.com>
2021-11-24 21:42:40 +0600

    Просто практика

--- JS/script.js
+++ JS/script.js
@@ -583,37 +583,118 @@
 
 
 
-let str = 'some';
-let strObj = new String(str);
+// let str = 'some';
+// let strObj = new String(str);
 
-console.log(typeof(str));
-console.log(typeof(strObj));
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-console.dir([1, 2, 3, 5]);
+// console.dir([1, 2, 3, 5]);
 
 
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function() {
-        console.log('Hello');
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Hello');
+//     }
+// };
+
+// const JonhTwo = Object.create(soldier);
+// // Современный метод прототипирования
+// JonhTwo.sayHello();
+
+// const Jonh = {
+//     health: 200
+// };
+
+// Jonh.__proto__ = soldier; // В современном коде не используется
+
+// Object.setPrototypeOf(Jonh, soldier); // Современный вариант использования прототипирования
+
+
+// console.log(Jonh.armor);
+// Jonh.sayHello();
+
+
+
+///////////////////////////////////
+///////////////////////////////////
+
+
+
+
+
+const personalMoveDB = {
+  count: 0,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+  start: function() {
+    personalMoveDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  
+    while (personalMoveDB.count == '' || personalMoveDB.count == null || isNaN(personalMoveDB.count)) {
+        personalMoveDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  
     }
+  },
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+    
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMoveDB.movies[a] = b;
+        console.log('done');
+      } else {
+        console.log('error');
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function() {
+    if (personalMoveDB.count < 10) {
+      console.log('Просмотренно довольно мало фильмов');
+    } else if (personalMoveDB.count >= 10 && personalMoveDB.count < 30) {
+      console.log('Вы классический зритель');
+    } else if (personalMoveDB.count >= 30) {
+      console.log('Вы киноман');
+    } else {
+      console.log('Произошла ошибка');
+    }
+  },
+  showMyDB: function(hidden) {
+    if (!hidden) {
+      console.log(personalMoveDB);
+    }
+  },
+  toggleVisibleMyDB: function() {
+    if (personalMoveDB.privat) {
+        personalMoveDB.privat = false;
+    } else {
+        personalMoveDB.privat = true;
+    }
+  },
+
+  writeYourGenres: function() {
+     for (let i = 1; i <= 3; i++) {
+        let genre = prompt( `Ваш любимый жанр под номером ${i}`);
+        
+        if (genre == '' || genre == null) {
+            console.log('Вы ввели некорректные данные или не ввлели вообще')
+            i--;
+        } else {
+            personalMoveDB.genres[i - 1] = genre;
+        } 
+     }
+     personalMoveDB.genres.forEach((item, i) => {
+        console.log(`Любимый жанр ${i + 1} - это ${item}`)
+     });
+
+  }
+  
 };
 
-const JonhTwo = Object.create(soldier);
-// Современный метод прототипирования
-JonhTwo.sayHello();
-
-const Jonh = {
-    health: 200
-};
-
-Jonh.__proto__ = soldier; // В современном коде не используется
-
-Object.setPrototypeOf(Jonh, soldier); // Современный вариант использования прототипирования
-
-
-console.log(Jonh.armor);
-Jonh.sayHello();
 
