66f95d2a3318f48f3f16288bf77ff55e691045cd
Kotoshilla <kotoshilla@gmail.com>
2021-11-30 14:48:02 +0600

    Навигация по DOM

--- JS/script.js
+++ JS/script.js
@@ -875,51 +875,80 @@
 
 
 
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
+// const btns = document.querySelectorAll('button'),
+//       overlay = document.querySelector('.overlay');
 
-// btn.onclick = function() {
-//     alert('Click');
-// };  //лучше не использвать такой варинат 
+// // btn.onclick = function() {
+// //     alert('Click');
+// // };  //лучше не использвать такой варинат 
 
-// btn.addEventListener('click', () => {
-//     alert('Click');
+// // btn.addEventListener('click', () => {
+// //     alert('Click');
+// // });
+
+// // btn.addEventListener('click', () => {
+// //     alert('Second Click');
+// // });
+
+// // let i = 0;
+// const deletElement = (event) => {
+//     console.log(event.currentTarget);
+//     console.log(event.type);
+//     // i++;
+//     // if (i == 1) {
+//     //     btn.removeEventListener('click', deletElement);
+//     // }
+// };
+
+// btn.addEventListener('click', (event) => {
+//     console.log(event.target);
+//     event.target.remove();
+//     console.log('Hover');
 // });
 
-// btn.addEventListener('click', () => {
-//     alert('Second Click');
+// btn.addEventListener('click', deletElement);
+// overlay.addEventListener('click', deletElement);
+// //Всплытие событий - когда срабатывает на самом вложеном элементе и вверх по иерархии
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', deletElement, {once: true});
 // });
 
-// let i = 0;
-const deletElement = (event) => {
-    console.log(event.currentTarget);
-    console.log(event.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deletElement);
-    // }
-};
+// const link = document.querySelector('a');
 
-btn.addEventListener('click', (event) => {
-    console.log(event.target);
-    event.target.remove();
-    console.log('Hover');
-});
+// link.addEventListener('click', (event) => {
+//     event.preventDefault();
 
-btn.addEventListener('click', deletElement);
-overlay.addEventListener('click', deletElement);
-//Всплытие событий - когда срабатывает на самом вложеном элементе и вверх по иерархии
-
-btns.forEach(btn => {
-    btn.addEventListener('click', deletElement, {once: true});
-});
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    console.log(event.target);
-});
+//     console.log(event.target);
+// });
 
 
+
+
+/* Навигация по DOM - элементам, data-атрибуты, преимущество for/of   */
+
+
+
+
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.body.childNodes); 
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+console.log(document.body.firstElementChild);
+
+console.log(document.querySelector('#current').parentNode);
+console.log(document.querySelector('#current').parentNode.parentNode);
+console.log(document.querySelector('#current').parentElement);
+
+ console.log(document.querySelector('[data-current="3"]').nextSibling);
+ console.log(document.querySelector('[data-current="3"]').previousSibling);
+console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    
+    console.log(node);
+}
