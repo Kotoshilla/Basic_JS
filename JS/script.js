d91ae23fe94618ea66a5b737517f29f521cdd72a
Kotoshilla <kotoshilla@gmail.com>
2021-12-13 13:07:59 +0600

    события на мобильных устройствах

--- JS/script.js
+++ JS/script.js
@@ -930,25 +930,56 @@
 
 
 
-console.log(document.body);
-console.log(document.documentElement);
-console.log(document.body.childNodes); 
-console.log(document.body.firstChild);
-console.log(document.body.lastChild);
-console.log(document.body.firstElementChild);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes); 
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.firstElementChild);
 
-console.log(document.querySelector('#current').parentNode);
-console.log(document.querySelector('#current').parentNode.parentNode);
-console.log(document.querySelector('#current').parentElement);
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
 
- console.log(document.querySelector('[data-current="3"]').nextSibling);
- console.log(document.querySelector('[data-current="3"]').previousSibling);
-console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+//  console.log(document.querySelector('[data-current="3"]').nextSibling);
+//  console.log(document.querySelector('[data-current="3"]').previousSibling);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == '#text') {
+//         continue;
+//     }
     
-    console.log(node);
-}
+//     console.log(node);
+// }
+
+
+
+
+/* События на мобильных устройствах */
+
+
+
+
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+
+        console.log('Start');
+        console.log(e.changedTouches);
+    });
+});
+
+
+// touches // Получение списка всех пальцев котрые на экране 
+// targetTouches // вывод пальца котрый взаимодействует именно с этим элементом
+// changedTouches // Список пальцев участвовуют в текушем событии
