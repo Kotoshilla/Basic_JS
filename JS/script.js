64d6693d5c436c37422c71a296574bac215ba43e
Kotoshilla <kotoshilla@gmail.com>
2021-12-13 16:42:08 +0600

    ClassLIst и делигирование событий

--- JS/script.js
+++ JS/script.js
@@ -991,17 +991,75 @@
 
 
 
-const p = document.querySelectorAll('p');
-console.log(p);
+// const p = document.querySelectorAll('p');
+// console.log(p);
 
 
 
-function loadScript(src) {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
+// function loadScript(src) {
+//     const script = document.createElement('script');
+//     script.src = src;
+//     script.async = false;
+//     document.body.append(script);
+// }
 
-loadScript("../JS/test.js");
-loadScript("../JS/some.js");
+// loadScript("../JS/test.js");
+// loadScript("../JS/some.js");
+
+
+
+
+/*Classlist И делигирование событий*/
+
+
+
+
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+// console.log(btns[1].classList.add('red')); //можно также через запятую добавлять классы 
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red');
+    } else {
+        btns[1].classList.remove('red');
+    }
+});
+// можно использовать и toggle
+
+// console.log(btns[0].className); //Устаревший метод
+//Лучше его не исползовать 
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.tagName == "BUTTON") {
+        console.log('Hello');
+    }
+}); //Будет рабоать даже если добавлять новые элементы 
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches("button.red")) {
+        console.log('Hello');
+    }
+});
+
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log('Hello');
+    });
+}); // На новых элементах не будет работать
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
+
